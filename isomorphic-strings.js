859ad82a4df4bf953cf82b4ec979a20769e84a33
Madej Pola <Pola.Madej@motorolasolutions.com>
2023-01-27 21:31:33 +0100

    resolved challenge

--- isomorphic-strings.js
+++ isomorphic-strings.js
@@ -3,9 +3,19 @@
  * @param {string} t
  * @return {boolean}
  */
- var isIsomorphic = function(s, t) {
-    let newS;
-    for (let i = 0; i < s.length - 1; i++) {
-        newS = s.replace(s[i], t[i]);
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) { return false; }
+
+    const map1 = [];
+    const map2 = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
+            return false;
+        }
+
+        map1[s.charAt(i)] = i +1;
+        map2[t.charAt(i)] = i +1;
     }
+    return true;
 };
