328f7d32be5d7360946fcdb1d2abd4d974d227b5
Madej Pola <Pola.Madej@motorolasolutions.com>
2023-01-27 21:09:52 +0100

    updated

--- find-pivot-index.js
+++ find-pivot-index.js
@@ -2,20 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-
- var pivotIndex = function (nums) {
+var pivotIndex = function(nums) {
     let leftSum = 0;
     let rightSum = 0;
 
-    nums.forEach((value) => rightSum += value);
+    nums.forEach((number) => rightSum += number);
 
-    for (let i = 0; i <= nums.length - 1; i++) {
+    for (let i = 0; i < nums.length; i++) {
         rightSum -= nums[i];
-
-        if (leftSum === rightSum) {
-            return i;
-        }
-
+        if (leftSum === rightSum) { return i; }
         leftSum += nums[i];
     }
 
