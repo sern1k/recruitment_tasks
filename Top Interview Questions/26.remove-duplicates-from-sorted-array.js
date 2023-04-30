61f462010a0849bca1a635cc5cb7c4357c07a387
Madej Pola <Pola.Madej@motorolasolutions.com>
2023-04-30 16:56:57 +0200

    Top Interview Questions

--- Top Interview Questions/26.remove-duplicates-from-sorted-array.js
+++ Top Interview Questions/26.remove-duplicates-from-sorted-array.js
@@ -2,6 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+// new solution
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            nums[++i] = nums[j];
+        }
+    }
+
+    return ++i;
+};
+
+// old solution
 var removeDuplicates = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === nums[i + 1]) {
